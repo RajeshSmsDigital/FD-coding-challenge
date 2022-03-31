f49dd46dd20c1451302733cee27d1304f09a9254
RajeshSmsDigital <rm@sms-digital.com>
2022-03-31 21:25:09 +0200

    intital commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        initial commit
+      </header>
+    </div>
+  );
+}
+
+export default App;
