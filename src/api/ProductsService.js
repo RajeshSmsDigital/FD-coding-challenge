1153f9b05aad308a4aaca6a4a3a47f8ed3d900b2
RajeshSmsDigital <rm@sms-digital.com>
2022-04-01 09:34:26 +0200

    dev: added responsive productlist

--- src/api/ProductsService.js
+++ src/api/ProductsService.js
@@ -5,6 +5,6 @@ export const productsWrapper = {
 };
 
 async function get() {
-  const response = await axios.get(process.env.REACT_APP_SCHEDULER_URL);
+  const response = await axios.get("https://s3-eu-west-1.amazonaws.com/fid-recruiting/fid-task-4-ffront-products.json");
   return response;
 }
--- src/components/ProductsList/ProductList.js
+++ src/components/ProductsList/ProductList.js
@@ -1,7 +1,39 @@
 import "./ProductList.scss";
+import React, { useEffect } from "react";
+import { productsWrapper } from "../../api/ProductsService";
 
 const ProductList = () => {
-  return <div>ProductList</div>;
+    const loadBacklogOrders = async () => {
+        try {
+            const res = await productsWrapper.get();
+            console.log(res);
+            if (res) {
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        loadBacklogOrders();
+    }, []);
+
+    return (
+        <div className="product-list">
+            <div className="filters">
+                <div> Size</div>
+                <div> Price</div>
+            </div>
+            <div className="products">
+                <div className="test"> 1</div>
+                <div className="test"> 2</div>
+                <div className="test"> 3</div>
+                <div className="test"> 4</div> 
+                <div className="test"> 5</div>
+                <div className="test"> 7</div>
+            </div>
+        </div>
+    );
 };
 
 export default ProductList;
